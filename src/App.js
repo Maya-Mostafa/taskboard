b0107eced430124be791d7ee0a89f0ee862c8497
MayaMostafa <mayamostafa@gmail.com>
2022-10-29 09:14:16 -0400

    adding router & pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+// React
+import { Navigate, Route, Routes } from 'react-router-dom';
+// Components 
+import Home from './pages/Home';
+import EditTask from './pages/EditTask';
+import NewTask from './pages/NewTask';
+import NotFound from './pages/NotFound';
+import TaskDetails from './pages/TaskDetails';
+// MUI
+import Container from '@mui/material/Container';
 
 function App() {
   return (
-    <div>Taskboard</div>
+    <>
+      <Container>
+        <Routes>
+          <Route path="/" element={<Navigate replace to="task-board" />} />
+          <Route path="/task-board/" element={<Home />} />
+          <Route path="/task-board/:colName/tasks/task-details/:taskName" element={<TaskDetails />} />
+          <Route path="/task-board/:colName/tasks/edit-task/:taskName" element={<EditTask />} />
+          <Route path="/task-board/:colName/tasks/new-task/" element={<NewTask />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Container>
+    </>
   );
 }
 
