b5ef945f2adf33f7bd3f11ef7d705b0474bbc377
MayaMostafa <mayamostafa@gmail.com>
2022-10-29 21:13:35 -0400

    adding App test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import {MemoryRouter} from 'react-router-dom';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('landing on a 404', () => {
+  const badRoute = '/anything/'
+
+  render(
+    <MemoryRouter initialEntries={[badRoute]}>
+      <App />
+    </MemoryRouter>,
+  )
+
+  expect(screen.getByText(/Page Not Found!/i)).toBeInTheDocument()
 });
+
