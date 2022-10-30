1754219b21fd5cfc23fc931d308555abd5089e50
MayaMostafa <mayamostafa@gmail.com>
2022-10-29 21:07:53 -0400

    404 page added

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from "react";
+import Container from "@mui/system/Container";
+import { Typography } from "@mui/material";
 
 const NotFound = () => {
-  return (
-    <div>NotFound</div>
-  )
-}
+	return <Container><Typography component='h2' variant='h4' padding={5}>Page Not Found!</Typography></Container>;
+};
 
-export default NotFound
+export default NotFound;
